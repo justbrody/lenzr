bbba271108426c331f584b2d752806ef4171b3fd
Christiaan Braad <c.braad@impulse-info.com>
2019-04-30 21:33:01 +0200

    Added tests, got 100% coverage.

--- src/createWithGlobalStore.test.js
+++ src/createWithGlobalStore.test.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { lensProp } from 'ramda'
 import { mount } from 'enzyme'
 import { createConnect } from './createConnect'
@@ -12,7 +12,7 @@ it('Can create HOC for providing the global store', () => {
     { token: 'asdfd', count: 10 },
     Ctx
   )
-  const Counter = ({ count }) => {
+  const Counter = ({ count, view, set, over }) => {
     expect(count).toBe(10)
 
     return <div>{count} </div>
@@ -25,6 +25,93 @@ it('Can create HOC for providing the global store', () => {
   mount(
     <App>
       <ConnectedCounter />
+    </App>
+  )
+})
+
+it('Use lens operation view', () => {
+  const Ctx = React.createContext()
+  const connect = createConnect(Ctx)
+  const dummyLens = lensProp('dummy')
+  const withGlobalStore = createWithGlobalStore(
+    { token: 'asdfd', dummy: 'Hallo dummy' },
+    Ctx
+  )
+  const Dummy = ({ view }) => {
+    expect(view(dummyLens)).toBe('Hallo dummy')
+
+    return <div />
+  }
+
+  const ConnectedDummy = connect()(Dummy)
+
+  const App = withGlobalStore(({ children }) => <div>{children}</div>)
+
+  mount(
+    <App>
+      <ConnectedDummy />
+    </App>
+  )
+})
+
+it('Use lens operation set', () => {
+  const Ctx = React.createContext()
+  const connect = createConnect(Ctx)
+  const dummyLens = lensProp('dummy')
+  const withGlobalStore = createWithGlobalStore(
+    { token: 'asdfd', dummy: 'Hallo dummy' },
+    Ctx
+  )
+  const Dummy = ({ dummy, set }) => {
+    const [first, isFirst] = useState(true)
+    useEffect(() => {
+      set(dummyLens, 'Hallo Lens')
+      isFirst(false)
+    }, [])
+
+    !first && expect(dummy).toBe('Hallo Lens')
+
+    return <div />
+  }
+
+  const ConnectedDummy = connect({ dummy: dummyLens })(Dummy)
+
+  const App = withGlobalStore(({ children }) => <div>{children}</div>)
+
+  mount(
+    <App>
+      <ConnectedDummy />
+    </App>
+  )
+})
+
+it('Use lens operation over', () => {
+  const Ctx = React.createContext()
+  const connect = createConnect(Ctx)
+  const dummyLens = lensProp('dummy')
+  const withGlobalStore = createWithGlobalStore(
+    { token: 'asdfd', dummy: 'Hallo dummy' },
+    Ctx
+  )
+  const Dummy = ({ dummy, over }) => {
+    const [first, isFirst] = useState(true)
+    useEffect(() => {
+      over(dummyLens, s => s.toUpperCase())
+      isFirst(false)
+    }, [])
+
+    !first && expect(dummy).toBe('HALLO DUMMY')
+
+    return <div>{dummy}</div>
+  }
+
+  const ConnectedDummy = connect({ dummy: dummyLens })(Dummy)
+
+  const App = withGlobalStore(({ children }) => <div>{children}</div>)
+
+  mount(
+    <App>
+      <ConnectedDummy />
     </App>
   )
 })
