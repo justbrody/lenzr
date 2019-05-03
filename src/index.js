d0b8c688d61c3e83ec23c938979d44906c2d04d3
Christiaan Braad <c.braad@impulse-info.com>
2019-05-03 13:45:40 +0200

    Rewriting the export

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import createGlobalStore from './createGlobalStore'
 import { lens, lensProp, lensPath } from 'ramda'
 
-export lens
-export lensProp
-export lensPath
+export { lens as lens }
+export { lensProp as lensProp }
+export { lensPath as lensPath }
 
 export default createGlobalStore
